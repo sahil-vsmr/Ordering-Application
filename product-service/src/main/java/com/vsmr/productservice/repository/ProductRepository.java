e42c0b637bd68d68b07a6e6855008c4451fd59b4
Sahil Agarwal <sahil.vsmr@gmail.com>
2023-01-23 13:14:36 +0530

    Initial Commit

---
+++ product-service/src/main/java/com/vsmr/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.vsmr.productservice.repository;
+
+import com.vsmr.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
